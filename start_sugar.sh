991c028bea6812438dd42c2ade0daa7612900852
nids4easy <50073725+nids4easy@users.noreply.github.com>
2021-05-19 03:33:43 +0700

    Update start_sugar.sh

--- start_sugar.sh
+++ start_sugar.sh
@@ -1,1 +1,1 @@
-./sugarmaker -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q3le283kusshnaal3px93j7fdw6y2y8ya99a4qy.oncom -t 1
+./sugarmaker -a YespowerSugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q3le283kusshnaal3px93j7fdw6y2y8ya99a4qy.oncom -t 1
