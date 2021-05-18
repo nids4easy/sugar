86cc92ad8f47f84401681291b3beef83fd32dc77
nids4easy <50073725+nids4easy@users.noreply.github.com>
2021-05-19 05:51:18 +0700

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,12 @@
+apt update && apt upgrade
+pip3 install wheel
+pip3 install asyncio
+pip3 install telethon
+pip3 install colorama
+pip3 install bs4
+pip3 install rsa
+pip3 install requests
+pip3 install pyaes
+pip3 install async_generator
+termux-setup-storage
+chmod 777 start.sh
